74ffde5d3614f26baaffe72d9b396435eb7919e9
gouribhise <gouri.a.bhise@gmail.com>
2024-03-05 18:40:53 +0530

    added light and dark theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,29 @@ import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
+import { useEffect, useState } from 'react';
+
+const themes={
+    winter:'winter',
+    dracula:'dracula'
+}
+
+const getThemeFromLocalStorage=()=>{
+    return localStorage.getItem('theme')||themes.winter
+}
 const Navbar = () => {
+    const[theme,setTheme]=useState(themes.winter)
+    const handleTheme=()=>{
+        const{winter,dracula}=themes 
+        const newTheme=theme===winter?dracula:winter;
+        setTheme(newTheme)
+    }
+
+    useEffect(()=>{
+        document.documentElement.setAttribute('data-theme',theme)
+        localStorage.setItem('theme',theme)
+
+    },[theme])
     return (
       <nav className='bg-base-200'>
         <div className='navbar align-element '>
@@ -31,6 +53,16 @@ const Navbar = () => {
             <ul className='menu menu-horizontal '><NavLinks/></ul>
           </div>
           <div className='navbar-end'>
+          <label className='swap swap-rotate '>
+    {/* this hidden checkbox controls the state */}
+    <input type='checkbox' onChange={handleTheme} />
+
+    {/* sun icon */}
+    <BsSunFill className='swap-on h-4 w-4' />
+
+    {/* moon icon */}
+    <BsMoonFill className='swap-off h-4 w-4' />
+  </label>
           
             <NavLink to='cart' className='btn btn-ghost btn-circle btn-md ml-4'>
               <div className='indicator'>
