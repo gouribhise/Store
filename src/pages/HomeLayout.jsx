5a67e92d05108203d32fc8bf14138de0adb6f4d1
gouribhise <gouri.a.bhise@gmail.com>
2024-03-05 14:57:00 +0530

    added header links

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import { Header } from '../components';
 const HomeLayout = () => {
     return  <>
- <nav>
-    <span class='text-4xl text-primary underline'>TheStore</span>
+ <Header/>
  
- </nav>
- 
-
+<section className='align-element py-20'>
     <Outlet />
+    </section>
     </>
   };
   export default HomeLayout;
