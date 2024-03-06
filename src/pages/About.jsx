ae840a96ee6a8718bfce2e5b31a0efc6170c292e
gouribhise <gouri.a.bhise@gmail.com>
2024-03-06 09:55:48 +0530

    added about Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,23 @@
 const About = () => {
-    return <h1 className='text-4xl'>About</h1>;
+    return  (
+      <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>    
+      <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl '>
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widget">
+              The Store
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum sapiente quasi ullam, amet voluptate omnis! Consequuntur inventore praesentium mollitia reprehenderit? Minus beatae, perferendis explicabo eius accusamus nulla porro sint iure!
+      </p>
+        </>
+    )
   };
   export default About;
