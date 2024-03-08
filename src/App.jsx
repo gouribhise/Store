5e759fbafab00b4ad777bfd40f124d38366a871e
gouribhise <gouri.a.bhise@gmail.com>
2024-03-08 16:31:12 +0530

    added login and demo user

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,9 @@ import { loader as SingleProductLoader } from './pages/SingleProduct';
 import {loader as productsLoader} from './pages/Products'
 
 import {action as registerAction} from './pages/Register'
+import {action as loginAction} from './pages/Login'
+
+import {store} from './store'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -61,6 +64,7 @@ const router = createBrowserRouter([
     path: '/login',
     element: <Login />,
     errorElement: <Error />,
+    action:loginAction(store)
   },
   {
     path: '/register',
